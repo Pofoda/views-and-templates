7f86373695e66ed1b34a5842123f5c2ec4289155
Pofoda <3011904@rts-365.dk>
2019-05-27 12:51:56 +0200

    variabler

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,14 +1,20 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home', {
+         "titel": "Hjem"
+      });
    });
 
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {
+         "titel":"Produkter"
+      });
    });
 
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', {
+         "titel": "Kontakt"
+      });
    });
 
 };
